893e72e237e5b8ab19a6592cd798c6b02f8290f9
bzorec1 <40613311+bzorec1@users.noreply.github.com>
2024-12-21 14:13:45 +0100

    added docker support

--- src/FinalSay.Worker/Program.cs
+++ src/FinalSay.Worker/Program.cs
@@ -3,43 +3,40 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using MassTransit;
 
-namespace FinalSay.Worker
+namespace FinalSay.Worker;
+
+public static class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            await CreateHostBuilder(args).Build().RunAsync();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddMassTransit(x =>
-                    {
-                        x.AddDelayedMessageScheduler();
-
-                        x.SetKebabCaseEndpointNameFormatter();
-
-                        // By default, sagas are in-memory, but should be changed to a durable
-                        // saga repository.
-                        x.SetInMemorySagaRepositoryProvider();
-
-                        var entryAssembly = Assembly.GetEntryAssembly();
-
-                        x.AddConsumers(entryAssembly);
-                        x.AddSagaStateMachines(entryAssembly);
-                        x.AddSagas(entryAssembly);
-                        x.AddActivities(entryAssembly);
-
-                        x.UsingRabbitMq((context, cfg) =>
-                        {
-                            cfg.UseDelayedMessageScheduler();
-
-                            cfg.ConfigureEndpoints(context);
-                        });
-                    });
-                });
+        await CreateHostBuilder(args).Build().RunAsync();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) => Host
+        .CreateDefaultBuilder(args)
+        .ConfigureServices((hostContext, services) =>
+        {
+            services.AddMassTransit(x =>
+            {
+                x.AddDelayedMessageScheduler();
+
+                x.SetKebabCaseEndpointNameFormatter();
+
+                x.SetInMemorySagaRepositoryProvider();
+
+                var entryAssembly = Assembly.GetEntryAssembly();
+
+                x.AddConsumers(entryAssembly);
+                x.AddSagaStateMachines(entryAssembly);
+                x.AddSagas(entryAssembly);
+                x.AddActivities(entryAssembly);
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.UseDelayedMessageScheduler();
+
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+        });
 }
